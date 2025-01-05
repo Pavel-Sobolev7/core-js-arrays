09e4897daad90056dfca2780254a2769b8fbfda4
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2025-01-06 00:44:13 +0300

    feat: finish is same length task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -150,8 +150,13 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length === 0) return true;
+
+  const { length } = arr[0];
+  return arr.every(function checkLength(str) {
+    return str.length === length;
+  });
 }
 
 /**
