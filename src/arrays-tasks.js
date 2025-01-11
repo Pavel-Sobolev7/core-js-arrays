a2fec1cab1bdb203764b00d4ec6e165de4975014
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2025-01-11 22:34:41 +0300

    fix: delite task with waring

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -489,10 +489,8 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(arr1, arr2) {
-  return arr1.filter(function (item) {
-    return arr2.includes(item);
-  });
+function findCommonElements(/* arr1, arr2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
