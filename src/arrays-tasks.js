a26e8b6b2addce3b39c87778afa8d13236df3df4
Pavel-Sobolev7 <pavel_sobolev_98@mail.ru>
2025-01-13 18:02:30 +0300

    feat: finish find common elements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -491,8 +491,9 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const set1 = new Set(arr1);
+  return arr2.filter((element) => set1.has(element));
 }
 
 /**
